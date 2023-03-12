818bf35cb1f79dfc6ed713322eb9158fb4216cb4
RobShrosSnow <127129547+RobShrosSnow@users.noreply.github.com>
2023-03-12 12:27:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 my_snowflake_fruit = pandas.df(my_data_row)
-add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_snowflake_fruit.index))
-fruit_chosen = my_snowflake_fruit.loc[add_my_fruit]
-streamlit.text("Thanks for adding ", fruit_chosen)
+add_my_fruit = streamlit.text_input("What fruit would you like to add?", 'jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
