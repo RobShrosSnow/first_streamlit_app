969437019fa0879fe7d6c4a0b209b6be675f4aaa
RobShrosSnow <127129547+RobShrosSnow@users.noreply.github.com>
2023-03-12 12:20:11 +0200

    Update streamlit_app.py
    testing text box picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_row.index))
+fruit_chosen = my_data_row.loc[add_my_fruit]
+streamlit.text("Thanks for adding ", fruit_chosen)
